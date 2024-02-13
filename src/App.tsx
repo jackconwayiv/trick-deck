c8a1d6f4ccff836e4320717f05879855d7fc4052
jackconwayiv <jackconwayiv@gmail.com>
2024-02-13 12:08:44 -0700

    draw a card of any color

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,79 @@
 import { useState } from "react";
-import card from "./mkm.js";
+import trickDeck from "./mkm.js";
 import "./styles.css";
 export default function App() {
-  const [results, setResults] = useState<number[]>([]);
+  const [log, setLog] = useState<string[]>([]);
 
-  const rollDice = (x: number) => {
-    const numbers = Array.from({ length: x }, () => rollD8());
-    setResults(numbers);
+  type Color = "W" | "U" | "B" | "R" | "G";
+
+  const colorDictionary = {
+    W: { color: "#FFFFEE", symbol: "☀️" },
+    U: { color: "#EEEEFF", symbol: "💧" },
+    B: { color: "#EEEEEE", symbol: "💀" },
+    R: { color: "#FFEEEE", symbol: "🔥" },
+    G: { color: "#EEFFEE", symbol: "🌳" },
+  };
+
+  const rollDice = (color: string) => {
+    if (rollD8() < 3) {
+      drawTrick(color);
+    } else setLog(["All Clear", ...log]);
   };
   const rollD8 = () => {
     return Math.floor(Math.random() * 8) + 1;
   };
 
-  const renderCard = () => {
-    return Object.values(card).join(" ");
+  const drawTrick = (color: string) => {
+    const selectedCards = trickDeck.filter((card) => card.cost.includes(color));
+    const index = Math.floor(Math.random() * selectedCards.length);
+    const card = selectedCards[index];
+    const newCard = {
+      name: card.name,
+      cost: `(${card.cost})`,
+      text: card.text,
+    };
+    const newLogLine = Object.values(newCard).join(" ");
+    setLog([newLogLine, ...log]);
+  };
+
+  const getColor = (line: string) => {
+    const chars = line.split("");
+    const costIndex = chars.indexOf(")");
+    return line[costIndex - 1];
+  };
+
+  const renderLog = () => {
+    return log.map((line, index) => (
+      <p
+        style={{
+          padding: "5px",
+          backgroundColor: line.includes(")")
+            ? colorDictionary[getColor(line) as Color].color
+            : "#FEFEFE",
+          border: index === 0 ? "1px black solid" : "1px gray dashed",
+        }}
+        key={index}
+      >
+        {line}
+      </p>
+    ));
   };
 
   const renderKeypad = () => {
     return (
       <div className="keypad" justify-self="center">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num, i) => {
+        {["W", "U", "B", "R", "G"].map((color, i) => {
           return (
-            <button onClick={() => rollDice(num)} key={i}>
-              {num}
+            <button
+              style={{
+                backgroundColor: colorDictionary[color as Color].color,
+                border: "1px black solid",
+              }}
+              onClick={() => rollDice(color as Color)}
+              key={i}
+            >
+              {colorDictionary[color as Color].symbol}
             </button>
-          );
-        })}
-      </div>
-    );
-  };
-  const renderRolls = () => {
-    return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        {results.map((num, i) => {
-          return (
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <span
-                key={i}
-                className="roll"
-                style={{
-                  color: num < 3 ? "red" : "black",
-                  backgroundColor: num < 3 ? "yellow" : "white",
-                  margin: "5px",
-                  padding: "5px",
-                }}
-              >
-                {num}
-              </span>
-              <span style={{ fontSize: "10px" }}> {num < 3 ? i + 1 : ""}</span>
-            </div>
           );
         })}
       </div>
@@ -62,11 +81,20 @@ export default function App() {
   };
   return (
     <div className="App">
-      <h1>⚡ Enemy Tricks 🎲</h1>
-      <h3>Click to roll that many d8's:</h3>
+      <h1>⚡ Instant -- Trick 🎲</h1>
+      <h3>Draw from the Trick Deck:</h3>
       {renderKeypad()}
-      {/* <h2>{renderCard()}</h2> */}
-      <h2>{renderRolls()}</h2>
+      <div
+        style={{
+          margin: "auto",
+          maxHeight: "65vh",
+          overflowY: "auto",
+          width: "90vw",
+          padding: "10px",
+        }}
+      >
+        {renderLog()}
+      </div>
     </div>
   );
 }
--- src/mkm.js
+++ src/mkm.js
@@ -1,9 +1,121 @@
-const spell = {
-  name: "Leave a Trace",
-  color: "white",
-  cost: "W",
-  type: "Instant - Trick",
-  text: "Exile target creature, then investigate. (Create a Clue token.)"
-}
+const whiteTricks = [
+  {
+    name: "Leave a Trace",
+    cost: "W",
+    text: "Exile target creature, then investigate.",
+  },
+  {
+    name: "Moment of Weakness",
+    cost: "1W",
+    text: "Prevent all combat damage target creature would deal this turn. A random Nexus with the lowest life total gains life equal to that creature's power.",
+  },
+  {
+    name: "Second Guess",
+    cost: "1W",
+    text: "Exile target creature, then return it to the battlefield tapped and removed from combat.",
+  },
+  {
+    name: "No Warrant",
+    cost: "4W",
+    text: "Exile target creature unless it is a Detective.",
+  },
+];
 
-export default spell;
+const blueTricks = [
+  {
+    name: "Loss of Control",
+    cost: "U",
+    text: "The next time target creature deals combat damage this turn, you mill cards equal to the damage dealt.",
+  },
+  {
+    name: "Coffee Break",
+    cost: "U",
+    text: "Tap target creature, remove it from combat, and put a stun counter on it.",
+  },
+  {
+    name: "Fleeting Glimpse",
+    cost: "U",
+    text: "Return target creature to its owner's hand.",
+  },
+  {
+    name: "False Lead",
+    cost: "2UU",
+    text: "Choose one -- Exile a Clue you control, or put target creature on top of its owner's library.",
+  },
+];
+
+const blackTricks = [
+  {
+    name: "Victimized",
+    cost: "1BB",
+    text: "Destroy target creature.",
+  },
+  {
+    name: "Blackmailed",
+    cost: "B",
+    text: "Choose one -- Discard a random card from your hand, or sacrifice a nontoken creature you control with the highest rarity.",
+  },
+  {
+    name: "Got a Bad Feeling",
+    cost: "1B",
+    text: "Target creature gets -X/-X until end of turn, where X is half the number of mana permanents you control, rounded up.",
+  },
+  {
+    name: "You're In It Now",
+    cost: "1B",
+    text: "Blight a (nonbasic) land you control (that isn't blighted). (Whenever it becomes tapped, you lose 1 life.)",
+  },
+];
+const redTricks = [
+  {
+    name: "Blowback",
+    cost: "2RR",
+    text: "The next time target creature deals combat damage this turn, it also deals that much damage to its controller.",
+  },
+  {
+    name: "Cast into Suspicion",
+    cost: "R",
+    text: "Suspect target creature. (It's suspected. It has menace and can't block.)",
+  },
+  {
+    name: "Collateral Damage",
+    cost: "R",
+    text: "Wound each creature you control that isn't attacking. (Put a -1/-1 counter on each of them.)",
+  },
+  {
+    name: "Take the Heat",
+    cost: "2R",
+    text: "Deal X damage to target creature, where X is half the number of your mana permanents, rounded up.",
+  },
+];
+const greenTricks = [
+  {
+    name: "Dogged Pursuit",
+    cost: "3GG",
+    text: "Encounter a number of tapped 1/1 white Dog creature tokens equal to target creature's toughness.",
+  },
+  {
+    name: "Led Astray",
+    cost: "G",
+    text: "Choose one -- Collect evidence 6, or prevent all damage target creature would deal this turn.",
+  },
+  {
+    name: "Unwanted Attention",
+    cost: "3G",
+    text: "Encounter a tapped 0/0 Elemental token with X +1/+1 counters on it, where X is the number of creatures plus the number of noncreature tokens you control.",
+  },
+  {
+    name: "Send into Hiding",
+    cost: "2G",
+    text: "Cloak target creature. (Turn it face-down. It can be turned face up at any time by paying its mana cost.)",
+  },
+];
+
+const trickDeck = [
+  ...whiteTricks,
+  ...blueTricks,
+  ...blackTricks,
+  ...redTricks,
+  ...greenTricks,
+];
+export default trickDeck;
