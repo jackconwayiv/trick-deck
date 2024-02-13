71d4a1e6b8be1469bfcdc213430cc7c6c8da0819
jackconwayiv <jackconwayiv@gmail.com>
2024-02-13 09:37:31 -0700

    transfer from codesandbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import card from "./mkm.js";
+import "./styles.css";
+export default function App() {
+  const [results, setResults] = useState<number[]>([]);
 
-function App() {
+  const rollDice = (x: number) => {
+    const numbers = Array.from({ length: x }, () => rollD8());
+    setResults(numbers);
+  };
+  const rollD8 = () => {
+    return Math.floor(Math.random() * 8) + 1;
+  };
+
+  const renderCard = () => {
+    return Object.values(card).join(" ");
+  };
+
+  const renderKeypad = () => {
+    return (
+      <div className="keypad" justify-self="center">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num, i) => {
+          return (
+            <button onClick={() => rollDice(num)} key={i}>
+              {num}
+            </button>
+          );
+        })}
+      </div>
+    );
+  };
+  const renderRolls = () => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        {results.map((num, i) => {
+          return (
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <span
+                key={i}
+                className="roll"
+                style={{
+                  color: num < 3 ? "red" : "black",
+                  backgroundColor: num < 3 ? "yellow" : "white",
+                  margin: "5px",
+                  padding: "5px",
+                }}
+              >
+                {num}
+              </span>
+              <span style={{ fontSize: "10px" }}> {num < 3 ? i + 1 : ""}</span>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>⚡ Enemy Tricks 🎲</h1>
+      <h3>Click to roll that many d8's:</h3>
+      {renderKeypad()}
+      {/* <h2>{renderCard()}</h2> */}
+      <h2>{renderRolls()}</h2>
     </div>
   );
 }
-
-export default App;
---
+++ src/mkm.js
@@ -0,0 +1,9 @@
+const spell = {
+  name: "Leave a Trace",
+  color: "white",
+  cost: "W",
+  type: "Instant - Trick",
+  text: "Exile target creature, then investigate. (Create a Clue token.)"
+}
+
+export default spell;
